010061d612684746b04bbebb8a8d8c7a50b82427
sdw98 <sdw9851@gmail.com>
2025-06-09 15:18:48 +0900

    문자열 매소드 이해

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
-    public static void main (String[] args){
-        int[] numbers = {2, 4, 6, 8, 10};
-        int sum=0;
+    public static void main (String[] args) {
+        String message = "Hello, Java!";
 
-        for(int i=0; i < numbers.length; i++){
-            sum += numbers[i];
-        }
-        System.out.println("총합 : " + sum);
+        System.out.println(message);
+        System.out.println(message.length()); //크기
+        System.out.println(message.toUpperCase()); //대문자변환
+        System.out.println(message.toLowerCase()); //소문자변환
+        System.out.println(message.indexOf("l")); //해당 글자 위치(중복이면 첫글자로)
+        System.out.println(message.charAt(5)); //한글자 출력
+
+        System.out.println(message.substring(2,6)); //2부터 6까지 출력
+        System.out.println(message.substring(message.length() - 3)); //뒤에서 3글자출력
     }
 }
