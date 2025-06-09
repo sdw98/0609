8c732b50f5b03f6fd9848e3ffc45b327636af8d6
sdw98 <sdw9851@gmail.com>
2025-06-09 11:30:50 +0900

    배열

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,12 @@
-import java.io.InvalidClassException;
-
 public class Main {
+    public static void main (String[] args){
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
 
-    public static void greet(){
-        System.out.println("Hello, world!");
-    }
-
-    public static void greet(String name){
-        System.out.println("Hello, " + name + "!");
-    }
-
-    public static void greet(String name, int age){
-        System.out.println(name + "나이 : " + age);
-    }
-
-
-
-    public static void main(String[] args) {
-        greet();
-        greet("Alice");
-        greet("Alice", 25);
+//      for (int i = 0; i < 5; i ++){
+//            System.out.println(numbers[i]);
+//        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
